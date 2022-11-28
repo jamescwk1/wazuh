3c15bf79a758875ead5d574608a41e1aaf658866
jamescwk1 <119279557+jamescwk1@users.noreply.github.com>
2022-11-28 13:17:57 +0800

    Update install.sh

---
+++ install.sh
@@ -0,0 +1,9 @@
+sudo apt-get install gnupg apt-transport-https -y
+sudo apt install curl
+#need root access
+sudo su
+curl -s https://packages.wazuh.com/key/GPG-KEY-WAZUH | gpg --no-default-keyring --keyring gnupg-ring:/usr/share/keyrings/wazuh.gpg --import && chmod 644 /usr/share/keyrings/wazuh.gpg
+echo "deb [signed-by=/usr/share/keyrings/wazuh.gpg] https://packages.wazuh.com/4.x/apt/ stable main" | tee -a /etc/apt/sources.list.d/wazuh.list
+
+#no root
+sudo apt-get update
